ff152b95b8c8e326b5b9ecaa5328a64131891441
Miyuru Dharmage <voxsar@gmail.com>
2022-11-20 14:22:45 +0530

    changes

--- .github/hooks/posthook.php
+++
@@ -1,5 +0,0 @@
-<?php
-//	if(isset($_REQUEST)){
-		echo file_put_contents("test.txt", json_encode($_REQUEST), FILE_APPEND);
-//	}ss
-?>
--- .github/hooks/posthook.sh
+++
@@ -1,8 +0,0 @@
-# Get new tags from remote
-git fetch --tags
-
-# Get latest tag name
-latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
-
-# Checkout latest tag
-git checkout $latestTag
